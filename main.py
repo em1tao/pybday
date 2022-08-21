977471a5d3fccf52f4707dd66e2d6ad9e1261862
hp <arslan.semenoff@gmail.com>
2022-08-21 18:03:00 +0500

    sql

--- main.py
+++ main.py
@@ -1,10 +1,20 @@
-import csv
-from datetime import datetime
+import sqlite3
+from datetime import datetime, timedelta
+
+
+conn = sqlite3.connect("db.db")
+cursor = conn.cursor() 
 
 today = datetime.today().date()
 
 def check():
-    friends = []
-    with open('db.csv') as f:
-        reader = csv.reader(f)
-        friend = reader.next()
+    sqlite_select_query = """SELECT * from bdays"""
+    cursor.execute(sqlite_select_query)
+    everyone = []
+    for i in cursor.fetchall():
+        days_left = today - datetime.strptime(i[2], "%d.%m.%Y").date()
+        print(days_left)
+
+
+
+check()
